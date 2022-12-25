12dc7f4e073c38ee44901bc7dcfc9442e619623c
VinhHP <vhpvn.dev@gmail.com>
2022-12-25 14:02:31 +0700

    Add jest to write unittest for typescript code

---
+++ test/video.controller.test.ts
@@ -0,0 +1,7 @@
+import { VideoController } from '../src/controllers';
+ 
+describe('testing video controller file', () => {
+  test('empty string should result in zero', () => {
+    expect(2 + 2).toBe(4);
+  });
+});
